5fef3ee15c45748d2a28f708ddcf79c2b5d6e294
Daniel Bulger <68453486+danielbulger@users.noreply.github.com>
2021-04-14 00:37:21 +1000

    Update xorwow.c

--- xorwow.c
+++ xorwow.c
@@ -3,19 +3,14 @@
 void xorwow_init(struct xorwow_state* state,const int seed[8])
 {
 	state->state[0] = _mm_set_epi32(seed[3], seed[2], seed[1], seed[0]);
-
 	state->state[1] = _mm_set_epi32(seed[7], seed[6], seed[5], seed[4]);
-
 	state->state[2] = _mm_setzero_si128();
-
 	state->state[3] = _mm_setzero_si128();
-
 	state->state[4] = _mm_setzero_si128();
 
 	// state->counter = (seed1 << 10) ^ (seed2 >> 4)
 	state->counter = _mm_xor_si128(
 		_mm_slli_epi32(state->state[0], 10),
-
 		_mm_srli_epi32(state->state[1], 4)
 	);
 }
@@ -23,15 +18,11 @@ void xorwow_init(struct xorwow_state* state,const int seed[8])
 __m128i xorwow(struct xorwow_state *state)
 {
 	__m128i t = state->state[4];
-
 	__m128i s = state->state[0];
 
 	state->state[4] = state->state[3];
-
 	state->state[3] = state->state[2];
-
 	state->state[2] = state->state[1];
-
 	state->state[1] = s;
 
 	// t >>= 2
